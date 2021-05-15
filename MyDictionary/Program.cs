051c9bd6d1355eea8ac1b812ca4614bf96d828d9
Zeynel Toplar <zt.2601@hotmail.com>
2021-05-15 21:21:56 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(0, "Engin");
+            myDictionary.Add(1, "Kerem");
+            myDictionary.Add(2, "Halil");
+            myDictionary.Add(3, "İlker");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
